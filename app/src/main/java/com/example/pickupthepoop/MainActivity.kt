af8f38a2cfdb162d98d90550daa87bc317d39f5d
SaharRamezani <saharramezanijolfaie@gmail.com>
2025-03-10 18:39:04 +0100

    Added poop images.

--- app/src/main/java/com/example/pickupthepoop/MainActivity.kt
+++ app/src/main/java/com/example/pickupthepoop/MainActivity.kt
@@ -4,12 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.pickupthepoop.ui.theme.PickUpThePoopTheme
 
@@ -19,26 +25,26 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PickUpThePoopTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-
-                }
+                SickDog(modifier = Modifier.fillMaxSize()
+                    .wrapContentSize(align = Alignment.Center))
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun SickDog(modifier: Modifier = Modifier) {
+    var numTapSickDog by remember { mutableStateOf(2) }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PickUpThePoopTheme {
-        Greeting("Android")
+    Column(modifier = modifier) {
+        Image(stringResource())
     }
 }
+
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    PickUpThePoopTheme {
+//        Greeting("Android")
+//    }
+//}
