adfe93c7e19d856535bb1c03ec4aacc40409b5ca
SaharRamezani <saharramezanijolfaie@gmail.com>
2025-03-10 18:26:00 +0100

    Start of the project.

--- app/src/main/java/com/example/pickupthepoop/MainActivity.kt
+++ app/src/main/java/com/example/pickupthepoop/MainActivity.kt
@@ -20,10 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             PickUpThePoopTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
                 }
             }
         }
