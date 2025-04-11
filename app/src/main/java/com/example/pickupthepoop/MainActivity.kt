137ff6b932843fe2ed35cd6446b85e8c15da1c90
SaharRamezani <saharramezanijolfaie@gmail.com>
2025-04-11 16:23:32 +0200

    now the app works well and a demo is added

--- app/src/main/java/com/example/pickupthepoop/MainActivity.kt
+++ app/src/main/java/com/example/pickupthepoop/MainActivity.kt
@@ -1,23 +1,19 @@
 package com.example.pickupthepoop
 
-import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -30,11 +26,8 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.core.content.ContentProviderCompat.requireContext
 import com.example.pickupthepoop.ui.theme.PickUpThePoopTheme
-import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -50,7 +43,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun SickDog(modifier: Modifier = Modifier) {
-    var numTapSickDog by remember { mutableStateOf(2) }
+    var numTapSickDog by remember { mutableStateOf((2..5).random()) }
     var step by remember { mutableStateOf(1) }
     var tapNum by remember { mutableStateOf(0) }
     val context = LocalContext.current
@@ -64,7 +57,7 @@ fun SickDog(modifier: Modifier = Modifier) {
                     if (tapNum == numTapSickDog) {
                         step++
                     }
-                    else if (tapNum == 1) {
+                    else if (tapNum == 1 || tapNum > numTapSickDog) {
                         numTapSickDog = (2..5).random()
                     }
                 },
@@ -78,32 +71,32 @@ fun SickDog(modifier: Modifier = Modifier) {
                 buttonClick = {
                     step++
                 },
-                context.resources.getIdentifier("poop" + (1..12).random().toString(),
+                context.resources.getIdentifier("poop" + (1..6).random().toString(),
                 "drawable", context.packageName),
                 step.toString(),
-                R.string.sick_dog,
+                R.string.pick_up,
             )
         }
         3 -> {
             ImageAndText(
                 buttonClick = {
-                    step++
+                    //step++
                 },
                 R.drawable.happy_dog,
                 step.toString(),
                 R.string.happy_dog,
             )
-        }
-        else -> {
-            ImageAndText(
-                buttonClick = {
-                    step++
-                },
-                R.drawable.dead_dog,
-                step.toString(),
-                R.string.dead_dog,
-            )
-        }
+       }
+//        else -> {
+//            ImageAndText(
+//                buttonClick = {
+//                    step = 1
+//                },
+//                R.drawable.dead_dog,
+//                step.toString(),
+//                R.string.dead_dog,
+//            )
+//        }
     }
 }
 
@@ -120,6 +113,7 @@ fun ImageAndText(buttonClick: () -> Unit, painter: Int, step: String, text: Int)
             modifier = Modifier
                 .padding(20.dp)
                 .clip(RoundedCornerShape(16.dp))
+                .size(300.dp)
                 .background(MaterialTheme.colorScheme.primary)
                 .padding(20.dp),
             onClick = buttonClick
@@ -133,14 +127,6 @@ fun ImageAndText(buttonClick: () -> Unit, painter: Int, step: String, text: Int)
         Text(
             text = stringResource(text)
         )
-
-//        if (step == "2") {
-//            Button() {
-//                Text(
-//                    text =
-//                )
-//            }
-//        }
     }
 }
 
